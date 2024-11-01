2e143f429f5a46c7f12e4e3ca2476c0403802d1c
Lena | Dunkel <7890309+DarkView@users.noreply.github.com>
2024-11-01 19:01:14 +0100

    Add status endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { WebsocketIncoming } from './connector/websocketIncoming';
+import express from 'express';
+import logging from "./util/Logging";
+const Log = logging("Status");
 
 const websocketIncoming = new WebsocketIncoming();
+
+const app = express();
+const port = 5101;
+
+app.get('/status', (req, res) => {
+    res.json({ status: 'UP' });
+});
+
+app.listen(port, () => {
+    Log.info(`Making status available on port ${port}`);
+});
