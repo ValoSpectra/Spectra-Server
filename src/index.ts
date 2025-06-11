1a6adca8fece37f304c830edb59101e35092c891
Dunkel <7890309+DarkView@users.noreply.github.com>
2025-06-11 22:57:40 +0200

    Add cors, a bit of logging

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import logging from "./util/Logging";
 import { MatchController } from "./controller/MatchController";
 import { PreviewHandler } from "./util/previews/PreviewHandler";
 import bodyParser from "body-parser";
+import cors from "cors";
 const Log = logging("Status");
 
 const websocketIncoming = new WebsocketIncoming();
 const previewHandler = PreviewHandler.getInstance(websocketIncoming);
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cors({ origin: "*" }));
 const port = 5101;
 
 app.get("/status", (req: Request, res: Response) => {
@@ -24,6 +25,7 @@ app.put("/createPreview", async (req: Request, res: Response) => {
 
 app.get("/preview/:previewCode", async (req: Request, res: Response) => {
   const previewCode = req.params.previewCode;
+  Log.info(`Received request for preview with code: ${previewCode}`);
   if (!previewCode || previewCode.length !== 6) {
     return res.status(400).json({ error: "Invalid preview code format" });
   }
