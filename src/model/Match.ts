c26f9bcd86ebe222fcce05901f5f8a2bd3232fc5
Paul Sommer <paul@sommers.dev>
2026-03-20 22:28:14 +0100

    fix: check for isSupporter, haha I forgot...

--- src/model/Match.ts
+++ src/model/Match.ts
@@ -95,7 +95,11 @@ export class Match {
     this.tools.watermarkInfo.customTextEnabled =
       this.tools.watermarkInfo.customTextEnabled && !!this.orgIsSupporter;
 
-    if (this.tools.roundWinBox.type == "sponsors" && this.tools.roundWinBox.sponsors.length > 1) {
+    if (
+      this.tools.roundWinBox.type == "sponsors" &&
+      this.tools.roundWinBox.sponsors.length > 1 &&
+      !this.orgIsSupporter
+    ) {
       this.tools.roundWinBox.sponsors = [this.tools.roundWinBox.sponsors[0]];
       this.tools.roundWinBox.sponsors[0].roundCeremony = ["all"];
       this.tools.roundWinBox.sponsors[0].wonTeam = "all";
